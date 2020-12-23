9c222772d5f0636fddceb355cdf301bc1f1e820e
FeralDaro <meral.nekrag@gmail.com>
2020-12-23 05:38:18 +0300

    Updates

---
+++ precache-manifest.e20dfcb0a8a1eb426bf1a96cb013b1f1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887e9c1cc08674bb292125467649c5cc",
+    "url": "/pet_project2/index.html"
+  },
+  {
+    "revision": "c19c3df25cd3d5b77228",
+    "url": "/pet_project2/static/css/3.aaf11b5b.chunk.css"
+  },
+  {
+    "revision": "77ab610908b0c7485e97",
+    "url": "/pet_project2/static/css/4.bcc1332a.chunk.css"
+  },
+  {
+    "revision": "9e337f7a59e426cd8efd",
+    "url": "/pet_project2/static/css/main.89c5fc26.chunk.css"
+  },
+  {
+    "revision": "4331296a9d960ba80e48",
+    "url": "/pet_project2/static/js/2.4424f3ca.chunk.js"
+  },
+  {
+    "revision": "c19c3df25cd3d5b77228",
+    "url": "/pet_project2/static/js/3.d5b5f52c.chunk.js"
+  },
+  {
+    "revision": "77ab610908b0c7485e97",
+    "url": "/pet_project2/static/js/4.e7bd6e00.chunk.js"
+  },
+  {
+    "revision": "9e337f7a59e426cd8efd",
+    "url": "/pet_project2/static/js/main.6e41dd94.chunk.js"
+  },
+  {
+    "revision": "3245b019629d0a460f53",
+    "url": "/pet_project2/static/js/runtime~main.873b1956.js"
+  },
+  {
+    "revision": "551cbe3f188248cdce8ef8537e7148b4",
+    "url": "/pet_project2/static/media/loader.551cbe3f.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/pet_project2/static/media/user.8b1dbb1f.png"
+  }
+]);
